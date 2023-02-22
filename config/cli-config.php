a6796aa20af4607d9e84154733698d4c57f0872f
Petar Stoqnov <desireon98@gmail.com>
2023-02-22 15:40:27 +0200

    Create the doctrine console.

---
+++ config/cli-config.php
@@ -0,0 +1,7 @@
+<?php
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+$container = require_once __DIR__ . '/bootstrap.php';
+
+return ConsoleRunner::createHelperSet($container[EntityManager::class]);
