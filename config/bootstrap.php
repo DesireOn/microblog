cce3a0c60169aa7e9880669e98c8492a92ede7a2
Petar Stoqnov <desireon98@gmail.com>
2023-02-22 15:40:09 +0200

    Add a bootstrap.php file so that entityManager service can be defined.

---
+++ config/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+use Doctrine\Common\Cache\Psr6\DoctrineProvider;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Slim\Container;
+
+$container = new Container(require __DIR__ . '/../config/settings.php');
+
+$container[EntityManager::class] = function (Container $c): EntityManager {
+    /** @var array $settings */
+    $settings = $c->get('settings');
+
+    // Use the ArrayAdapter or the FilesystemAdapter depending on the value of the 'dev_mode' setting
+    // You can substitute the FilesystemAdapter for any other cache you prefer from the symfony/cache library
+    $cache = $settings['doctrine']['dev_mode'] ?
+        DoctrineProvider::wrap(new ArrayAdapter()) :
+        DoctrineProvider::wrap(new FilesystemAdapter(directory: $settings['doctrine']['cache_dir']));
+
+    $config = Setup::createAttributeMetadataConfiguration(
+        $settings['doctrine']['metadata_dirs'],
+        $settings['doctrine']['dev_mode'],
+        null,
+        $cache
+    );
+
+    return EntityManager::create($settings['doctrine']['connection'], $config);
+};
+
+return $container;
