9fda4a2abb6010aabbc9fe799544f4cf0cb65342
Petar Stoqnov <desireon98@gmail.com>
2023-02-25 12:50:03 +0200

    Instantiate slim framework in index.php with basic settings.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require 'vendor/autoload.php';
+use Slim\App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/../app/settings.php';
+$app = new App($settings);
+
+try {
+    $app->run();
+} catch (Throwable $e) {
+    echo $e->getMessage();
+}
