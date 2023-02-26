a5703e68605910f41dad7afa75119a08f2bcc704
Petar Stoqnov <desireon98@gmail.com>
2023-02-26 12:12:00 +0200

    Create a new 'admin' route group. Move the login route inside that group.

--- app/routes.php
+++ app/routes.php
@@ -4,5 +4,7 @@ use App\Controller\Admin\LoginController;
 use Slim\App;
 
 return function (App $app) {
-    $app->map(['GET', 'POST'],'/admin/login', LoginController::class . ':login');
+    $app->group('/admin', function (App $app) {
+        $app->map(['GET', 'POST'],'/login', LoginController::class . ':login');
+    });
 };
