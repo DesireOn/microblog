4b16f1f0c23d1103540376d75437be958a61f8fc
Petar Stoqnov <desireon98@gmail.com>
2023-02-26 12:23:04 +0200

    Create a new middleware class that will be activated when user lands on an "admin" route group.

---
+++ app/src/Middleware/AdminMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Middleware;
+
+use App\Service\Auth;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * A middleware class that is being activated when user visits "/admin/*".
+ * It redirects user to homepage whenever he/she is not logged in.
+ */
+class AdminMiddleware
+{
+    private Auth $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param callable $next
+     * @return ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface
+    {
+        if ($this->auth->isLogged() === false) {
+            $response->withRedirect('/admin/login');
+        }
+
+        return $next($request, $response);
+    }
+}
