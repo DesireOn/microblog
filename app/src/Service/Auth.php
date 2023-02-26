66c0d3f906f456c5765a3eb5e4043be7717b574e
Petar Stoqnov <desireon98@gmail.com>
2023-02-26 09:09:46 +0200

    Create a function checkCredentials for checking whether provided username and password are correct based on records in the user table. In case credentials are correct, save 'is_logged' flag in the session of the visitor.

--- app/src/Service/Auth.php
+++ app/src/Service/Auth.php
@@ -2,14 +2,43 @@
 
 namespace App\Service;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManager;
 
 class Auth
 {
     private EntityManager $entityManager;
+    private string $email;
+    private string $password;
 
     public function __construct(EntityManager $entityManager)
     {
         $this->entityManager = $entityManager;
+    }
+
+    public function login(string $email, string $password): void
+    {
+        $this->email = $email;
+        $this->password = $password;
+
+        if ($this->checkCredentials()) {
+            $_SESSION['is_logged'] = true;
+        }
+    }
+
+    private function checkCredentials(): bool
+    {
+        /** @var User $user */
+        $user = $this->entityManager->getRepository(User::class)->findOneBy([
+            'email' => $this->email
+        ]);
+        if (!is_null($user)) {
+            $hashedPassword = $user->getPassword();
+            if (password_verify($this->password, $hashedPassword)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
