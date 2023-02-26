cad8fd1f29333db45b023240d9b3f35990f081a6
Petar Stoqnov <desireon98@gmail.com>
2023-02-26 12:23:36 +0200

    Activate AdminMiddleware when user visits admin group.

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Controller\Admin\LoginController;
+use App\Middleware\AdminMiddleware;
 use Slim\App;
 
 return function (App $app) {
     $app->group('/admin', function (App $app) {
         $app->map(['GET', 'POST'],'/login', LoginController::class . ':login');
-    });
+    })->add(AdminMiddleware::class);
 };
