9fb36817b6f0416f05c987ffc2a1a0bcb5075772
Petar Stoqnov <desireon98@gmail.com>
2023-02-26 07:46:43 +0200

    Require the dependencies.php file in cli-config.php. Now the container is being passed.

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,7 +1,11 @@
 <?php
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Slim\Container;
 
-$container = require_once __DIR__ . '/../app/bootstrap.php';
+$container = new Container(require __DIR__ . '/../app/settings.php');
+
+$bootstrap = require_once __DIR__ . '/../app/dependencies.php';
+$bootstrap($container);
 
 return ConsoleRunner::createHelperSet($container[EntityManager::class]);
