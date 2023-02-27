3a9d3dc6e41ab9e6d765d52106e714b23805d180
Petar Stoqnov <desireon98@gmail.com>
2023-02-27 06:58:33 +0200

    Add proper routes for BlogPostController.

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controller\Admin\BlogPostController;
 use App\Controller\Admin\UserController;
 use App\Controller\LoginController;
 use App\Middleware\AdminMiddleware;
@@ -9,11 +10,19 @@ return function (App $app) {
     $app->map(['GET', 'POST'],'/login', LoginController::class . ':login')
         ->setName('login');
     $app->group('/admin', function (App $app) {
+        // Users routes
         $app->get('/users/list', UserController::class . ':list')
             ->setName('admin_users_list');
         $app->map(['GET', 'POST'], '/users/create', UserController::class . ':create')
             ->setName('admin_users_create');
         $app->map(['GET', 'POST'], '/users/update/{id}', UserController::class . ':update')
             ->setName('admin_users_update');
+        // Blog posts routes
+        $app->get('/blog-posts/list', BlogPostController::class . ':list')
+            ->setName('admin_blog_posts_list');
+        $app->map(['GET', 'POST'], '/blog-posts/create', BlogPostController::class . ':create')
+            ->setName('admin_blog_posts_create');
+        $app->map(['GET', 'POST'], '/blog-posts/update/{id}', BlogPostController::class . ':update')
+            ->setName('admin_blog_posts_update');
     })->add(AdminMiddleware::class);
 };
